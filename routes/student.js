5b540f42e796072c32842eb400a9e45ca3f9a0b9
Sreeni <sreenivas9chinta@gmail.com>
2022-11-21 13:25:34 +0530

    Added ReactJS Server App

---
+++ routes/student.js
@@ -0,0 +1,54 @@
+const { query } = require("express");
+var express = require("express");
+var router = express.Router();
+var mysql = require("mysql");
+
+
+/* GET users listing. */
+router.get("/std-reg", (req, res, next) => {
+  const { name, loc } = req.query;
+  var output = `Hello i am  ${name} from ${loc} `;
+  res.send(output);
+});
+
+
+//POST
+router.post("/reg-std", (req, res, next) => {
+  let conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "sree",
+    database: "nodedb",
+  });
+
+  const { name, uid, pwd, gender, hobbies, country, address } = req.body;
+
+  console.log(
+    "**** " + name + uid + pwd + gender + hobbies + country + address
+  );
+
+  conn.connect((err, succ) => {
+    if (err) {
+      res.send("Data Not inserted Error: " + err);
+    } else {
+      let q = `insert into student(name, uid, pwd, gender, hobbies, country, address)  values( '${name}', '${uid}', '${pwd}', '${gender}','${hobbies}','${country}', '${address}')`;
+
+      console.log("query : " + q);
+
+      conn.query(q, (err, succ) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(succ);
+        }
+      });
+
+     // res.send("Data Inserted Successfully : " + succ + "   Query: " + query);
+    }
+  });
+
+  //res.send(`POST reg-std is called... ${name} ${loc} `);
+});
+
+module.exports = router;
